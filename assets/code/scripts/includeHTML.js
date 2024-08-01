2df4401baadf52c3cf46b3b4f24e78cfabdcf8ae
Muzammal Anwar <muzammalanwar556@gmail.com>
2024-08-01 05:34:52 +0200

    privacy policy and legal notice are complete and little bit of project structure change

---
+++ assets/code/scripts/includeHTML.js
@@ -0,0 +1,28 @@
+// Hier ist die navbar javascript Datei
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+includeHTML();
+
+// Copy these lines to insert Navbar in each html file:
+    // <div w3-include-html="navbarTemplate.html"></div>
+    // <script src="../scripts/includeHTML.js"></script>
