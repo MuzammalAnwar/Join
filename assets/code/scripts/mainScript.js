991108e64288327769d7d3c11819c71bb542f64f
Muzammal Anwar <muzammalanwar556@gmail.com>
2024-08-09 17:54:18 +0200

    getTask and postTask successfully implemented

--- assets/code/scripts/mainScript.js
+++ assets/code/scripts/mainScript.js
@@ -1,6 +1,6 @@
 const URL = 'https://join-301-default-rtdb.europe-west1.firebasedatabase.app/users'
 let task = {
-    "title": "Task 1",
+    "title": "Task 1 TO UPLOAD",
     "description": "Create JSON",
     "assignedTo": {},
     "dueDate": {
@@ -14,23 +14,39 @@ let task = {
 }
 
 function init() {
-    postTask("/addedTasks/done", task);
+    postTask("users/addedTasks/done/tasks/firstTask", task);
+    getTask("users");
+}
+
+async function getTask(path = "") {
+    try {
+        let response = await fetch(`https://join-301-default-rtdb.europe-west1.firebasedatabase.app/${path}.json`, {
+            method: "GET"
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let responseData = await response.json();
+        console.log('Response Data:', responseData);
+    } catch (error) {
+        console.error('Error getting task:', error);
+    } finally {
+        console.log('Request completed');
+    }
 }
 
 async function postTask(path = "", data) {
     try {
-        let response = await fetch(URL + path, {
+        let response = await fetch(`https://join-301-default-rtdb.europe-west1.firebasedatabase.app/${path}.json`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         });
-
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
         let responseData = await response.json();
         console.log('Response Data:', responseData);
     } catch (error) {
