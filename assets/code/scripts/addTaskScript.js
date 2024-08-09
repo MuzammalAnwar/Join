5a5e0a1479e88c1b20877596567be71442c0e89d
Kenan Günes <kguenes0@gmail.com>
2024-08-09 10:14:25 +0200

    addTask css edited and input content in json with JS

--- assets/code/scripts/addTaskScript.js
+++ assets/code/scripts/addTaskScript.js
@@ -1,13 +1,15 @@
 let tasks = [];
 
 
-function addToTask() {
+function addToTask(event) {
+    event.preventDefault();
+
     let title = document.getElementById('title');
     let description = document.getElementById('description');
     let assigned = document.getElementById('assigned');
     let dueDate = document.getElementById('due-date');
     let category = document.getElementById('category');
-    let subTasks = document.getElementById('subTasks');
+    let subTasks = document.getElementById('subtasks');
 
     let task = {
         "title": title.value,
@@ -15,11 +17,12 @@ function addToTask() {
         "assigned": assigned.value,
         "due-date": dueDate.value,
         "category": category.value,
-        "subTasks": subTasks.value
+        "subtasks": subTasks.value
     };
 
     tasks.push(task);
     console.log(tasks);
+    
     title.value = '';
     description.value = '';
     assigned.value = '';
