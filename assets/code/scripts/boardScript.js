84fcb86d8a16bbc6997ae95db46cefba3c8a1b25
Kenan Günes <kguenes0@gmail.com>
2024-08-16 11:58:07 +0200

    Header updated

--- assets/code/scripts/boardScript.js
+++ assets/code/scripts/boardScript.js
@@ -12,7 +12,7 @@ function renderTasks(category, id) {
         let keys = Object.keys(taskArray);
         for (let i = 0; i < keys.length; i++) {
             let task = taskArray[keys[i]];
-            htmlContent.innerHTML += /*HTML*/`
+            htmlContent.innerHTML += `
                 <div class="task">
                     <p id="category" class='${returnClass(task.category)}'>${task.category}</p>
                     <div class="taskTitleAndDescription">
