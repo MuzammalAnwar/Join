986b22b34ed1cf708b3291111c4886bad913d1f9
Muzammal Anwar <muzammalanwar556@gmail.com>
2024-08-19 14:06:33 +0200

    subtask inputfield improved

--- assets/code/scripts/addTaskScript.js
+++ assets/code/scripts/addTaskScript.js
@@ -76,23 +76,77 @@ function changeImgSource(id, src) {
 }
 
 function addSubtask() {
-    let input = document.getElementById('subtasks')
-    let subtaskList = document.getElementById('subtaskList')
+    let input = document.getElementById('subtasks');
+    let subtaskList = document.getElementById('subtaskList');
     if (input.value !== '') {
         subtaskList.innerHTML = '';
-        subtasks.push(input.value)
-        for (let i = 0; i < subtasks.length; i++) {
-            subtaskList.innerHTML += /*HTML*/`
-            <ul>
-                <li><p>${subtasks[i]}</p></li>
-            </ul>
-            `;
-        }
+        subtasks.push(input.value);
+        renderSubtasks();
         input.value = '';
     } else {
-        alert('Write something')
+        alert('Write something');
     }
 }
+
+function addSubtask() {
+    let input = document.getElementById('subtasks');
+    let subtaskList = document.getElementById('subtaskList');
+    if (input.value !== '') {
+        subtaskList.innerHTML = '';
+        subtasks.push(input.value);
+        renderSubtasks();
+        input.value = '';
+    } else {
+        alert('Write something');
+    }
+}
+
+function renderSubtasks() {
+    let subtaskList = document.getElementById('subtaskList');
+    subtaskList.innerHTML = '';
+
+    subtasks.forEach((subtask, i) => {
+        subtaskList.innerHTML += /*HTML*/`
+            <li class="subtaskListItem" id="subtaskListItem${i}">
+                <p class="subtaskListText">${subtask}</p>
+                <div class="subtaskIcons">
+                    <img onclick="editSubtask(${i})" src="../../img/subtaskEditIcon.png" class="subtaskIcon" alt="Edit Icon">
+                    <img onclick="deleteSubtask(${i})" src="../../img/subtaskTrashIcon.png" class="subtaskIcon" alt="Trash Icon">
+                </div>
+            </li>
+        `;
+    });
+}
+
+function editSubtask(index) {
+    let subtaskListItem = document.getElementById(`subtaskListItem${index}`);
+    let subtaskText = subtasks[index];
+
+    // Replace the text with an input field and icons
+    subtaskListItem.innerHTML = /*HTML*/`
+        <div class="subtaskEditContainer">
+            <input class="editInput" type="text" value="${subtaskText}" class="subtaskEditInput" id="subtaskEditInput${index}">
+            <div class="subtaskEditSeparator"></div>
+            <div class="subtaskEditIcons">
+                <img onclick="renderSubtasks()" src="../../img/subtaskTrashIcon.png" class="subtaskIcon" alt="Cancel Icon">
+                <img onclick="saveSubtask(${index})" src="../../img/subtaskAddIcon.png" class="subtaskIcon" alt="Save Icon">
+            </div>
+        </div>
+    `;
+}
+
+function saveSubtask(index) {
+    let input = document.getElementById(`subtaskEditInput${index}`);
+    subtasks[index] = input.value;
+    renderSubtasks(); // Re-render the list with the updated value
+}
+
+function deleteSubtask(index) {
+    subtasks.splice(index, 1);
+    renderSubtasks(); // Re-render the list after deleting the item
+}
+
+
 
 function handleButtonClick(clickedIconId, originalSrc, hoverSrc, otherIconIds) {
     otherIconIds.forEach(function (id) {
@@ -106,16 +160,6 @@ function handleButtonClick(clickedIconId, originalSrc, hoverSrc, otherIconIds) {
     toggleIcon(clickedIconId, originalSrc, hoverSrc);
 }
 
-function toggleIcon(iconId, originalSrc, hoverSrc) {
-    let icon = document.getElementById(iconId);
-    let currentSrc = icon.src.split('/').pop();
-    if (currentSrc === originalSrc.split('/').pop()) {
-        icon.src = hoverSrc;
-    } else {
-        icon.src = originalSrc;
-    }
-
-}
 
 function clearSubtasks() {
     document.getElementById('subtaskList').innerHTML = '';
@@ -129,5 +173,37 @@ function checkLoginStatus() {
         window.location.href = 'loadingSpinner.html';
     }
 }
+function toggleIcon(iconId, originalSrc, hoverSrc) {
+    let icon = document.getElementById(iconId);
+    let currentSrc = icon.src.split('/').pop();
+    if (currentSrc === originalSrc.split('/').pop()) {
+        icon.src = hoverSrc;
+    } else {
+        icon.src = originalSrc;
+    }
+}
+
+function toggleInputIcons(inputId, addIconId, cancelIconId, saveIconId) {
+    const input = document.getElementById(inputId);
+    const addIcon = document.getElementById(addIconId);
+    const cancelIcon = document.getElementById(cancelIconId);
+    const saveIcon = document.getElementById(saveIconId);
+
+    input.addEventListener('focus', function () {
+        addIcon.classList.add('hidden');
+        cancelIcon.classList.remove('hidden');
+        saveIcon.classList.remove('hidden');
+    });
+
+    input.addEventListener('blur', function () {
+        addIcon.classList.remove('hidden');
+        cancelIcon.classList.add('hidden');
+        saveIcon.classList.add('hidden');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    toggleInputIcons('subtasks', 'subtaskAdd', 'subtaskCancel', 'subtaskSave');
+});
 
 window.addEventListener('load', checkLoginStatus);
