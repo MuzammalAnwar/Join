49a06416e81de07ab6723788c3d27324aac8410c
Muzammal Anwar <muzammalanwar556@gmail.com>
2024-09-02 13:50:15 +0200

    edit overlay change contacts implemented

--- assets/code/scripts/editOverlay.js
+++ assets/code/scripts/editOverlay.js
@@ -1,41 +1,41 @@
-document.querySelector('.edit_button').addEventListener('click', function() {
-    const taskId = currentTaskId; 
-    const taskTitle = document.getElementById('tall_task_overlay_title').textContent; 
+document.querySelector('.edit_button').addEventListener('click', function () {
+    const taskId = currentTaskId;
+    const taskTitle = document.getElementById('tall_task_overlay_title').textContent;
     const taskDescription = document.getElementById('task_description').textContent;
     const taskDueDate = document.getElementById('task_due_date').textContent;
     const taskPriority = document.getElementById('prio_name').textContent.toLowerCase();
-    const assignedTo = [...document.querySelectorAll('.profile-circle')].map(el => el.textContent); 
-    showEditOverlay(taskId, taskTitle, taskDescription, taskDueDate, taskPriority, assignedTo); 
+    const assignedTo = [...document.querySelectorAll('.profile-circle')].map(el => el.textContent);
+    showEditOverlay(taskId, taskTitle, taskDescription, taskDueDate, taskPriority, assignedTo);
 });
 
 function showEditOverlay(taskId, title, description, dueDate, priority, assignedTo) {
-    hideTallTaskOverlay(); 
-    
+    hideTallTaskOverlay();
+
     let tallOverlay = document.getElementById('tall_task_overlay_background');
     if (tallOverlay) {
-        tallOverlay.style.display = 'none'; 
+        tallOverlay.style.display = 'none';
     }
-    
+
     let editOverlay = document.getElementById('edit_task_overlay_background');
     if (editOverlay) {
-        editOverlay.style.display = 'flex'; 
+        editOverlay.style.display = 'flex';
         document.getElementById('edit_title').value = title || 'No title provided';
         document.getElementById('edit_description').value = description || 'No description provided';
-        
+
         if (dueDate) {
             const localDate = new Date(dueDate);
             const offsetDate = new Date(localDate.getTime() - localDate.getTimezoneOffset() * 60000);
             const formattedDate = offsetDate.toISOString().substr(0, 10);
             document.getElementById('edit_due_date').value = formattedDate;
         } else {
-            document.getElementById('edit_due_date').value = ''; 
+            document.getElementById('edit_due_date').value = '';
         }
 
         setPriorityInEditOverlay(priority);
 
         const assignedContainer = document.querySelector('.selected-contacts');
-        assignedContainer.innerHTML = ''; 
-        
+        assignedContainer.innerHTML = '';
+
         if (assignedTo && assignedTo.length > 0) {
             assignedTo.forEach(contact => {
                 assignedContainer.innerHTML += `
@@ -53,6 +53,7 @@ function showEditOverlay(taskId, title, description, dueDate, priority, assigned
     } else {
         console.error('Edit overlay element not found');
     }
+    renderAssignedContactsInEditOverlay(taskId)
 }
 
 function saveTaskChanges(event) {
@@ -81,18 +82,18 @@ function saveTaskChanges(event) {
 
     const taskId = currentTaskId;
 
-    console.log('Saving task:', taskId, updatedTask); 
+    console.log('Saving task:', taskId, updatedTask);
 
     fetchTask(`/tasks/${taskId}`, updatedTask, 'PUT')
-    .then(responseData => {
-        console.log('Response data:', responseData); 
-        updateTaskInBoard(taskId, updatedTask);
-        hideEditOverlay();
-    })
-    .catch(error => {
-        console.error('Fehler beim Speichern der Änderungen:', error);
-        alert('Es gab einen Fehler beim Speichern der Änderungen. Bitte versuche es erneut.');
-    });
+        .then(responseData => {
+            console.log('Response data:', responseData);
+            updateTaskInBoard(taskId, updatedTask);
+            hideEditOverlay();
+        })
+        .catch(error => {
+            console.error('Fehler beim Speichern der Änderungen:', error);
+            alert('Es gab einen Fehler beim Speichern der Änderungen. Bitte versuche es erneut.');
+        });
 }
 
 
@@ -107,16 +108,16 @@ function updateTaskInBoard(taskId, updatedTask) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('edit_urgentIcon').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('edit_urgentIcon').addEventListener('click', function () {
         setPriorityInEditOverlay('urgent');
     });
 
-    document.getElementById('edit_mediumIcon').addEventListener('click', function() {
+    document.getElementById('edit_mediumIcon').addEventListener('click', function () {
         setPriorityInEditOverlay('medium');
     });
 
-    document.getElementById('edit_lowIcon').addEventListener('click', function() {
+    document.getElementById('edit_lowIcon').addEventListener('click', function () {
         setPriorityInEditOverlay('low');
     });
 });
@@ -155,9 +156,9 @@ function setPriorityInEditOverlay(selectedPriority) {
 }
 
 function getRandomRgbColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
@@ -168,9 +169,94 @@ function getInitials(name) {
 function hideEditOverlay() {
     let editOverlay = document.getElementById('edit_task_overlay_background');
     if (editOverlay) {
-        editOverlay.style.display = 'none'; 
+        editOverlay.style.display = 'none';
     } else {
         console.error('Edit overlay element not found');
     }
 }
+
+async function renderAssignedContactsInEditOverlay(taskID) {
+    let selectWrapper = document.querySelector('.select-options');
+    let html = '';
+    let contacts = await fetchTask(`/${userID}/contacts`, null, 'GET');
+    let assignedContacts = await highlightAlreadyAssignedContacts(taskID);
+
+    if (contacts) {
+        let keys = Object.keys(contacts);
+        for (let i = 0; i < keys.length; i++) {
+            let contact = contacts[keys[i]];
+            let isAssigned = assignedContacts.includes(contact.name);
+            html += /*HTML*/`
+                <div class="custom-option" data-value="${contact.name}" style="${returnStyleBasedOnContactAssignStatus(isAssigned)}">
+                    <div class="profileCircleAndName"> 
+                        <div class="profile-circle" style="background-color: ${contact.color};">
+                            ${getInitials(contact.name)}
+                        </div>
+                        <span>${contact.name}</span>
+                    </div>
+                    <input type="checkbox" value="${contact.name}" ${isAssigned ? 'checked' : ''}>
+                </div>
+            `;
+        }
+    } else {
+        html = `No contacts available`;
+    }
+    selectWrapper.innerHTML = html;
+    addCheckboxListeners();
+}
+
+function addCheckboxListeners() {
+    let checkboxes = document.querySelectorAll('.custom-option input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            updateContactStyle(this);
+        });
+        updateContactStyle(checkbox);
+    });
+}
+
+async function highlightAlreadyAssignedContacts(taskID) {
+    let assignedContacts = await fetchTask(`/${userID}/addedTasks/${taskID}/assigned`, null, 'GET');
+    return assignedContacts || [];
+}
+
+function returnStyleBasedOnContactAssignStatus(isAssigned) {
+    return `background-color: ${isAssigned ? 'rgba(42, 54, 71, 1)' : 'white'}; color: ${isAssigned ? 'white' : 'black'}`;
+}
+
+function updateContactStyle(checkbox) {
+    const customOption = checkbox.closest('.custom-option');
+    if (checkbox.checked) {
+        customOption.style.backgroundColor = 'rgba(42, 54, 71, 1)';
+        customOption.style.color = 'white';
+    } else {
+        customOption.style.backgroundColor = 'white';
+        customOption.style.color = 'black';
+    }
+}
+
+async function updateAssignedContacts(taskID) {
+    let checkedContacts = document.querySelectorAll('.custom-option input[type="checkbox"]:checked');
+    let assignedContacts = [];
+    checkedContacts.forEach(checkbox => {
+        assignedContacts.push(checkbox.value);
+    });
+    try {
+        await fetchTask(`/${userID}/addedTasks/${taskID}/assigned`, assignedContacts, 'PUT');
+    } catch (error) {
+        console.error('Error updating contacts:', error);
+    }
+}
+
+document.querySelector('.select-trigger').addEventListener('click', function () {
+    const selectBox = document.querySelector('.select-box');
+    selectBox.classList.toggle('active');
+});
+
+window.addEventListener('click', function (e) {
+    const selectBox = document.querySelector('.select-box');
+    if (!selectBox.contains(e.target) && selectBox.classList.contains('active')) {
+        selectBox.classList.remove('active');
+    }
+});
 
